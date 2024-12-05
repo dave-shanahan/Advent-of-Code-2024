aa042282498e0a8d548ac02d286416f88590cfe3
Dave Shanahan <hello@daveshanahan.com>
2024-12-05 13:43:12 +0000

    Created a better solution for Day 2 part 1

--- 2/2.rb
+++ 2/2.rb
@@ -1,51 +1,30 @@
 safe_reports = 0
 
-def valid_sort?(report)
-  if report[0] < report[1]
-    report.each_index do |i|
-      puts "Index: #{i}"
-      if i != report.length - 1
-        if (report[i] == report[i+1])
-          next
-        end
-        if (report[i] > report[i+1])
-          return false
-        end
-      end
-    end
-    true
-  end
+def sorted_correctly?(ascending, current, post)
+  return current < post if ascending
 
-  report.each_index do |i|
-    puts "Index: #{i}"
-    if i != report.length - 1
-      if (report[i] == report[i+1])
-        next
-      end
-      if (report[i] < report[i+1])
-        return false
-      end
-    end
-  end
-  true
+  current > post
 end
 
-# def valid_sort?(report)
-#   sorts = [report.sort { |a, b| a <=> b }, report.sort { |a, b| b <=> a }]
-#   true if sorts.include?(report)
-# end
+def safe_report?(report)
+  ascending = report[0] < report[1]
 
-def contains_valid_levels?(report)
   report.each_index do |index|
-    if index != 0
-      prior_difference = (report[index] - report[index-1]).abs
-      return false unless within_permitted_range?(prior_difference)
-    end
+    next if index == report.length - 1
 
-    if index != report.length - 1
-      post_difference = (report[index] - report[index+1]).abs
-      return false unless within_permitted_range?(post_difference)
-    end
+    current = report[index]
+    post = report[index + 1]
+
+    return false unless sorted_correctly?(ascending, current, post)
+
+    q = (current - post).abs
+    return false unless within_permitted_range?(q)
+
+    next unless index != 0
+
+    prior = report[index - 1]
+    p = (current - prior).abs
+    return false unless within_permitted_range?(p)
   end
   true
 end
@@ -54,17 +33,9 @@ def within_permitted_range?(difference)
   (difference > 0) && (difference <= 3)
 end
 
-File.foreach('example_input.txt') do |line|
+File.foreach('input.txt') do |line|
   report = line.split.map(&:to_i)
-
-  puts "Report: #{report}"
-  puts "Report length: #{report.length}"
-  puts "Report valid? #{valid_sort?(report)}"
-  
-  # if valid_sort?(report) && contains_valid_levels?(report)
-  #   safe_reports += 1
-  # end
-
+  safe_reports += 1 if safe_report?(report)
 end
 
 puts "Safe Reports: #{safe_reports}"
